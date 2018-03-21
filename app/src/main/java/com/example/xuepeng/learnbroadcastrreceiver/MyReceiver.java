f21ac8d47b4028cccea01c8edb704fb05213fbfc
xuepeng <helloxuepeng@gmail.com>
2018-03-21 23:00:24 +0800

    Android 广播接收器 BroadcastReceiver

---
+++ app/src/main/java/com/example/xuepeng/learnbroadcastrreceiver/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.xuepeng.learnbroadcastrreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        System.out.println(" 接收到消息 ");
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
